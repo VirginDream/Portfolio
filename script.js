35bcafc36b5570705d8489064c635971f171cc72
VirginDream <khaldrowood@gmail.com>
2024-09-03 11:40:35 +0200

    New branch

---
+++ script.js
@@ -0,0 +1,22 @@
+ let darkMode= false;
+
+ function changerDarkMode() {
+    if(darkMode) {
+
+    darkMode= false;
+    document.documentElement.style.setProperty("--text-color","black");
+    document.documentElement.style.setProperty("--background-color","#efe7e5");
+    document.getElementById("dark-light-mode").innerHTML = "Dark Mode"; 
+} else {
+    darkMode= true;
+    document.documentElement.style.setProperty("--text-color","white");
+    document.documentElement.style.setProperty("--background-color","black");
+    document.getElementById("dark-light-mode").innerHTML = "Light Mode";
+}
+ }
+
+
+ let livresTotal=500;
+ livresTotal+=50;
+ livresTotal
+console.log
