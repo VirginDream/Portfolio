064885f6267a6a576c075edc95b7573aad7fcb71
VirginDream <104904148+VirginDream@users.noreply.github.com>
2024-09-03 17:27:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,12 @@
     darkMode= false;
     document.documentElement.style.setProperty("--text-color","black");
     document.documentElement.style.setProperty("--background-color","#efe7e5");
-    document.getElementById("dark-light-mode").innerHTML = "Dark Mode"; 
+    document.getElementById("dark-light-mode").innerHTML = "Thème Sombre"; 
 } else {
     darkMode= true;
     document.documentElement.style.setProperty("--text-color","white");
-    document.documentElement.style.setProperty("--background-color","black");
-    document.getElementById("dark-light-mode").innerHTML = "Light Mode";
+    document.documentElement.style.setProperty("--background-color","#464858");
+    document.getElementById("dark-light-mode").innerHTML = "Thème Clair";
 }
  }
 
